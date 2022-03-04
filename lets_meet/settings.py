d8146d605b2611389dde4892636a50f82e08e501
Tamir Sror <tamirsr@mta.ac.il>
2022-03-04 13:59:45 +0200

    URL path changes
    - a few changes in the urls path to accomedate the new Room model
    - an addition of RoomConfig to the Installed Apps in the settings page
    - added static files for the welcome_page

--- lets_meet/settings.py
+++ lets_meet/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rooms.apps.RoomsConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
