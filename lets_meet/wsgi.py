8995687d330ca7fa21ffe689e8e6fdec7af9e5b7
Tamir Sror <tamirsr@mta.ac.il>
2022-03-04 10:53:17 +0200

    Project Name Change
    - changing the name of the project from meeting_room to lets_meet
    - as a result all the files in the old meeting_room folder has been
      changed accordingly to be set to the new name

--- lets_meet/wsgi.py
+++ lets_meet/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for meeting_room project.
+WSGI config for lets_meet project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meeting_room.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lets_meet.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meeting_room.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lets_meet.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
