37789ec5362a567cbdf802a48157a7927526ebe4
BelikovYoav <yoavbl@mta.ac.il>
2022-03-26 17:15:03 +0300

    Add tests for Event model
    add validators for date_time_start and date_time_end fields in Event model
    change TIME_ZONE to 'Asia/Jerusalem'

--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Event(models.Model):
@@ -26,6 +27,19 @@ class Event(models.Model):
         choices=Colors.choices,
         default=Colors.BLACK,
     )
+
+    def clean(self):
+        if ((self.date_time_end is None and self.date_time_start is not None)
+           or (self.date_time_start is None and self.date_time_end is not None)):
+            raise ValidationError('start and end date_time - one field not filled')
+
+        if (self.date_time_start is not None and self.date_time_end is not None
+           and self.date_time_start >= self.date_time_end):
+            raise ValidationError('date_time_start is equal or greater than date_time_end')
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return f"{self.title} - {self.date_time_start}"
--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,57 @@
-# from django.test import TestCase
+from django.core.exceptions import ValidationError, NON_FIELD_ERRORS
+from django.utils import timezone
+from .models import Event
+from datetime import datetime
 
-# Create your tests here.
+import pytest
+
+
+DATE_TIME_EARLY = datetime(2022, 3, 24, 12, 12, 12, 0, tzinfo=timezone.utc)
+DATE_TIME_LATER = datetime(2022, 3, 24, 14, 12, 12, 0, tzinfo=timezone.utc)
+ERROR = 'ValidationError not raised'
+
+
+@pytest.fixture
+def new_event():
+    return Event(title='new_title', location='new_location', description='new_description',
+                 date_time_start=DATE_TIME_EARLY, date_time_end=DATE_TIME_LATER)
+
+
+@pytest.mark.django_db
+class TestEvent:
+
+    def test_create_delete_event(self, new_event):
+        new_event.save()
+        assert new_event in Event.objects.all()
+        new_event.delete()
+        assert new_event not in Event.objects.all()
+
+    def test_blank_title_event(self):
+        try:
+            Event.objects.create()
+            assert False, ERROR
+        except ValidationError as e:
+            assert 'This field cannot be blank.' in e.message_dict['title']
+
+    def test_blank_date_time_event(self, new_event):
+        try:
+            new_event.date_time_start, new_event.date_time_end = DATE_TIME_EARLY, None
+            new_event.save()
+            assert False, ERROR
+        except ValidationError as e:
+            assert 'start and end date_time - one field not filled' in e.message_dict[NON_FIELD_ERRORS]
+
+        try:
+            new_event.date_time_start, new_event.date_time_end = None, DATE_TIME_EARLY
+            new_event.save()
+            assert False, ERROR
+        except ValidationError as e:
+            assert 'start and end date_time - one field not filled' in e.message_dict[NON_FIELD_ERRORS]
+
+    def test_start_bigger_equal_end_event(self, new_event):
+        try:
+            new_event.date_time_start, new_event.date_time_end = DATE_TIME_LATER, DATE_TIME_EARLY
+            new_event.save()
+            assert False, ERROR
+        except ValidationError as e:
+            assert 'date_time_start is equal or greater than date_time_end' in e.message_dict[NON_FIELD_ERRORS]
