9a74778972997facd47098b95b52372c3b191522
Ron H <hachmonron@gmail.com>
2022-03-28 15:22:41 +0300

    basic possible meeting model

--- events/models.py
+++ events/models.py
@@ -1,7 +1,16 @@
 from django.db import models
-
+from django_db.models.signals import pre_save
 
 class EventParticipant(models.Model):
     event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     is_creator = models.BooleanField(default=False, null=False)
+
+
+
+
+class PossibleMeeting(models.Model):
+    participant_id=models.ForeignKey(Event, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_time_start = models.DateTimeField()
+    date_time_end = models.DateTimeField()
