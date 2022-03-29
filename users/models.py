792e0c8ca86f3a21783af0a28d50fe3ca0f9ff94
YaelD <63968945+YaelD@users.noreply.github.com>
2022-03-29 14:54:41 +0300

    Validation change
    - added password validations and an improved phone number validation

--- users/models.py
+++ users/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
-from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.password_validation import validate_password
 
 
 def validate_email_addr(email):
@@ -14,34 +15,47 @@ def validate_email_addr(email):
 
 
 def validate_phone_number(phone_number):
+    def get_phone_number():
+        return "+972" + phone_number[1:] if is_international else phone_number
+
+    is_international = False
+
     if phone_number is not None:
         if phone_number[0] == '+':
+            is_international = True
             if phone_number[:4] != '+972':
                 raise ValidationError(
                     _(f'{phone_number} is not supported'),
                     params={'phone_number': phone_number},
                 )
-            if len(phone_number) != 13:
-                raise ValidationError(
-                    _(f'{phone_number} is not a valid phone number. The number should have 13 digits'),
-                    params={'phone_number': phone_number},
-                )
-            phone_number = phone_number[1:]
-        else:
-            if phone_number[:2] != '05':
-                raise ValidationError(
-                    _(f'{phone_number} is not a valid phone number. The number should start with 05'),
-                    params={'phone_number': phone_number},
-                )
-            if len(phone_number) != 10:
-                raise ValidationError(
-                    _(f'{phone_number} is not a valid phone number. The number should have 10 digits'),
-                    params={'phone_number': phone_number},
-                )
+            phone_number = "0" + phone_number[4:]
+
+        if phone_number[:2] != '05':
+
+            raise ValidationError(
+                _(f'{get_phone_number()} is not a valid phone number. The number should start with 05'),
+                params={'phone_number': get_phone_number()},
+            )
+
+        if phone_number[2] == '7':
+            raise ValidationError(
+                _(f'{get_phone_number()} is not a valid phone number. ' +
+                  'The number should start with 05 and a number that is not 7'),
+                params={'phone_number': get_phone_number()},
+            )
+
+        if len(phone_number) != 10:
+            digit_amount = 13 if is_international else 10
+
+            raise ValidationError(
+                _(f'{get_phone_number()} is not a valid phone number. The number should have {digit_amount} digits'),
+                params={'phone_number': get_phone_number()},
+            )
+
         if not phone_number.isnumeric():
             raise ValidationError(
-                _(f'{phone_number} has at least one character'),
-                params={'phone_number': phone_number},
+                _(f'{get_phone_number()} has at least one character'),
+                params={'phone_number': get_phone_number()},
             )
 
 
@@ -62,6 +76,7 @@ class User(AbstractUser):
 
 
 def user_fields_validation(sender, instance, **kwargs):
+    validate_password(instance.password)
     validate_email_addr(instance.email)
     validate_phone_number(instance.phone_number)
 
