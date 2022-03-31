faef781c750a98519f25b424237118af6be57dc9
Yael Davidov <yael.davidov97@gmail.com>
2022-03-31 13:31:25 +0300

    Migration File Change:
    - change in migartion file numbered 0008 in the help text
    
    Signed-off-by: Yael Davidov <yael.davidov97@gmail.com>

--- users/migrations/0007_alter_user_groups_alter_user_is_active_and_more.py
+++ users/migrations/0007_alter_user_groups_alter_user_is_active_and_more.py
@@ -15,24 +15,27 @@ class Migration(migrations.Migration):
             model_name='user',
             name='groups',
             field=models.ManyToManyField(
-                blank=True, help_text=('The groups this user belongs to. A user',
-                                       ' will get all permissions granted to each of their groups.'),
+                blank=True,
+                help_text='The groups this user belongs to.' +
+                          ' A user will get all permissions granted to each of their groups.',
                 related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups'),
         ),
         migrations.AlterField(
             model_name='user',
             name='is_active',
             field=models.BooleanField(
-                default=True, help_text=('Designates whether this user should be',
-                                         ' treated as active. Unselect this instead of deleting accounts.'),
+                default=True,
+                help_text='Designates whether this user should be' +
+                          ' treated as active. Unselect this instead of deleting accounts.',
                 verbose_name='active'),
         ),
         migrations.AlterField(
             model_name='user',
             name='is_superuser',
             field=models.BooleanField(
-                default=False, help_text=('Designates that this user has all',
-                                          ' permissions without explicitly assigning them.'),
+                default=False,
+                help_text='Designates that this user has all' +
+                          ' permissions without explicitly assigning them.',
                 verbose_name='superuser status'),
         ),
         migrations.AlterField(
