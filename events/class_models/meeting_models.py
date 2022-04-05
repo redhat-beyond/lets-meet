2764e79654320a427d5a201c1eb8917835dcaaab
Ron Hachmon <88894070+RonHachmon@users.noreply.github.com>
2022-04-05 17:10:11 +0300

    Arrange models (#40)
    * possible meeting model
    
    -adding possible meeting model
    -adding tests to possible meeting
    -adding hard coded db data
    
    * adding tests for uniqe possible meeting
    reformating date
    
    * arranging model to seperate files
    
    -arranging models to seperate file
    -and adding gitignore to ignore .idea
    
    * setting tests under classes
    and moving the Color model to be outside the event model
    
    * using pytest.raises in event test
    removing unnecessary @pytest.mark.django_db marks
    
    * removing duplicate .idea  from gitignore

---
+++ events/class_models/meeting_models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from .event_models import validate_date_time, time_format
+from .participant_model import EventParticipant
+from django.core.exceptions import ValidationError
+
+
+class PossibleMeeting(models.Model):
+    participant_id = models.ForeignKey(EventParticipant, on_delete=models.CASCADE)
+    date_time_start = models.DateTimeField()
+    date_time_end = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f"{self.participant_id} - {time_format(self.date_time_start)} - {time_format(self.date_time_end)}"
+
+    def clean(self) -> None:
+        validate_date_time(self.date_time_start, self.date_time_end)
+        validate_unique_possible_meeting(self.participant_id, self.date_time_start, self.date_time_end)
+        return super().clean()
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        return super().save(*args, **kwargs)
+
+
+def validate_unique_possible_meeting(participant_id, date_time_start, date_time_end):
+    if PossibleMeeting.objects.filter(
+            participant_id=participant_id,
+            date_time_start=date_time_start,
+            date_time_end=date_time_end
+    ):
+        raise ValidationError('meeting hours already exists')
---
+++ events/class_tests/event_tests.py
@@ -0,0 +1,60 @@
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from ..models import Event, time_format
+from datetime import datetime
+
+import pytest
+
+DATE_TIME_START = datetime(2022, 3, 24, 12, 12, 12, 0, tzinfo=timezone.utc)
+DATE_TIME_END = datetime(2022, 3, 24, 14, 12, 12, 0, tzinfo=timezone.utc)
+TITLE = 'new_title'
+LOCATION = 'new_location'
+DESCRIPTION = 'new_description'
+
+
+@pytest.fixture
+def new_event():
+    return Event(
+        title=TITLE, location=LOCATION, description=DESCRIPTION,
+        date_time_start=DATE_TIME_START, date_time_end=DATE_TIME_END
+    )
+
+
+@pytest.fixture
+def persist_event(db, new_event):
+    new_event.save()
+    return new_event
+
+
+def create_event(title, date_time_start, date_time_end):
+    return Event(title=title, date_time_start=date_time_start, date_time_end=date_time_end)
+
+
+@pytest.mark.django_db
+class TestEvent():
+
+    def test_add_event(self, persist_event):
+        assert persist_event in Event.objects.all()
+
+    def test_delete_event(self, persist_event):
+        persist_event.delete()
+        assert persist_event not in Event.objects.all()
+
+    def test_exist_event(self):
+        assert Event.objects.get(title='event1')
+        assert Event.objects.get(title='event2')
+        assert Event.objects.get(title='event3')
+
+    @pytest.mark.parametrize('title, date_time_start, date_time_end, expected_error', [
+        (None, DATE_TIME_START, DATE_TIME_END, 'Title cannot be blank'),
+        ('', DATE_TIME_START, DATE_TIME_END, 'Title cannot be blank'),
+        (TITLE, None, DATE_TIME_END, 'Starting date cannot be blank'),
+        (TITLE, DATE_TIME_START, None, 'Ending date cannot be blank'),
+        (TITLE, DATE_TIME_START, DATE_TIME_START,
+         f'{time_format(DATE_TIME_START)} must be smaller than {time_format(DATE_TIME_START)}'),
+        (TITLE, DATE_TIME_END, DATE_TIME_START,
+         f'{time_format(DATE_TIME_END)} must be smaller than {time_format(DATE_TIME_START)}')
+    ])
+    def test_invalidation(self, title, date_time_start, date_time_end, expected_error):
+        with pytest.raises(ValidationError, match=expected_error):
+            create_event(title, date_time_start, date_time_end).save()
