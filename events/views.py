f255ee97362d3c65d5f3b6971dbde769bdba5003
BelikovYoav <yoavbl@mta.ac.il>
2022-04-12 17:37:31 +0300

    Add create event page
    when submitting create_event form, a new event is created in events model
    and a new data is created with current event and
    current user in eventParticipant model.

--- events/views.py
+++ events/views.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from events.models import Event
@@ -45,3 +46,33 @@ def update_event(request, pk):
 
     return render(request, 'events/create_event.html',
                   {'form': form, 'title': 'Update Event'})
+=======
+from django.shortcuts import render, redirect
+from events.forms import EventCreationForm
+from django.contrib import messages
+from events.class_models.participant_model import EventParticipant
+
+
+def create_event(request):
+    if not request.user.is_authenticated:
+        return redirect("welcome_page")
+
+    if request.method == 'POST':
+        print(request.POST)
+        form = EventCreationForm(request.POST)
+        if form.is_valid():
+            print("Valid form")
+            new_event = form.save()
+
+            event_participant = EventParticipant(event_id=new_event, user_id=request.user, is_creator=True)
+            event_participant.save()
+
+            title = form.cleaned_data.get('title')
+            messages.success(request, f'Event: {title} created!')
+            return redirect('welcome_page')
+
+    else:
+        form = EventCreationForm()
+
+    return render(request, 'events/create_event.html', {'form': form})
+>>>>>>> b81ad64 (Add create event page)
