7e02ad40d41f767c28a70b1ddd6601013e0154c4
BelikovYoav <yoavbl@mta.ac.il>
2022-04-16 22:11:42 +0300

    add login required

--- events/views.py
+++ events/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from events.forms import EventCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+REDIRECT_PAGE = 'welcome_page'
 
 
+@login_required(login_url=REDIRECT_PAGE)
 def create_event(request):
-    if not request.user.is_authenticated:
-        return redirect("welcome_page")
 
     if request.method == 'POST':
         print(request.POST)
@@ -16,7 +18,7 @@ def create_event(request):
 
             title = form.cleaned_data.get('title')
             messages.success(request, f'Event: {title} created!')
-            return redirect('welcome_page')
+            return redirect(REDIRECT_PAGE)
 
     else:
         form = EventCreationForm(user_id=request.user)
