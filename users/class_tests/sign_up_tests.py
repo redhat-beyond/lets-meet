269e58817f26a5aa5ada2e4fae28b6d68a8a534f
Tamir Sror <tamirsr@mta.ac.il>
2022-04-18 17:19:49 +0300

    constants added

--- users/class_tests/sign_up_tests.py
+++ users/class_tests/sign_up_tests.py
@@ -1,8 +1,11 @@
 import pytest
 from users.models import User
-from .constants import LOGIN_HTML_PATH
 from pytest_django.asserts import assertTemplateUsed
 from users.forms import MyUserCreationForm as SignUpForm
+from .constants import (
+    LOGIN_HTML_PATH, VALID_EMAIL, VALID_USERNAME,
+    VALID_PASSWORD, NOT_VALID_PASSWORD
+)
 
 
 @pytest.mark.django_db
@@ -20,11 +23,11 @@ class TestSignUp:
 
     @pytest.mark.parametrize("invalid_user_credentials", [
         # username cannot be None
-        {'username': None, 'email': 'valid@mta.ac.il', 'password1': 'pw123123', 'password2': 'pw123123'},
+        {'username': None, 'email': VALID_EMAIL, 'password1': VALID_PASSWORD, 'password2': VALID_PASSWORD},
         # password cannot be None
-        {'username': 'valid_username', 'email': 'valid@mta.ac.il', 'password1': None, 'password2': None},
+        {'username': VALID_USERNAME, 'email': VALID_EMAIL, 'password1': None, 'password2': None},
         # passowrds do not match
-        {'username': 'valid_username', 'email': 'valid@mta.ac.il', 'password1': '000000000', 'password2': 'pw123123'}
+        {'username': VALID_USERNAME, 'email': VALID_EMAIL, 'password1': NOT_VALID_PASSWORD, 'password2': VALID_PASSWORD}
     ])
     def test_sign_up_invalid(self, invalid_user_credentials):
         form = SignUpForm(data=invalid_user_credentials)
