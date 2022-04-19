716fb253142ae931b95cb3f31a1b17205845b480
Yael Davidov <yael.davidov97@gmail.com>
2022-04-19 23:42:50 +0300

    save work before merge

--- events/urls.py
+++ events/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path("create/", views.create_event, name="create_event"),
     path('update/<str:pk>', views.update_event, name='update_event'),
+    path("create/meeting", views.create_meeting, name="create_meeting")
 ]
--- events/views.py
+++ events/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from events.models import Event
@@ -46,31 +45,3 @@ def update_event(request, pk):
 
     return render(request, 'events/create_event.html',
                   {'form': form, 'title': 'Update Event'})
-=======
-from django.shortcuts import render, redirect
-from events.forms import EventCreationForm
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-
-REDIRECT_PAGE = 'welcome_page'
-
-
-@login_required(login_url=REDIRECT_PAGE)
-def create_event(request):
-
-    if request.method == 'POST':
-        print(request.POST)
-        form = EventCreationForm(request.POST, user_id=request.user)
-        if form.is_valid():
-            print("Valid form")
-            form.save()
-
-            title = form.cleaned_data.get('title')
-            messages.success(request, f'Event: {title} created!')
-            return redirect(REDIRECT_PAGE)
-
-    else:
-        form = EventCreationForm(user_id=request.user)
-
-    return render(request, 'events/create_event.html', {'form': form})
->>>>>>> b81ad64 (Add create event page)
