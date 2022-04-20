90b890285306bb172a7c7ae5ce9f1046a490777e
Ravid Yael <ravid.yael.7@gmail.com>
2022-04-20 17:19:52 +0300

    add using of the user's test fixtures

--- events/tests.py
+++ events/tests.py
@@ -5,6 +5,7 @@ from .class_models.participant_model import EventParticipant
 from .class_models.event_models import Event, Colors
 from .forms import EventCreationForm
 from users import models
+from users.class_tests.users_tests import *  # noqa: F401 F403
 import pytest
 
 # form preferences -> new event preferences
@@ -14,22 +15,6 @@ DESCRIPTION = 'new_form_description'
 DATE_TIME_START = "11:11 PM 11-Nov-2011"
 DATE_TIME_END = "12:12 PM 12-Dec-2012"
 COLOR = Colors.BLACK
-# form's 'test user' preferences -> new event creator preferences
-NAME = "form_test_user"
-EMAIL = "form_test_user@gmail.com"
-PASSWORD = "form_test_U$er123"
-PHONE_NUM = "+972544651893"
-
-
-@pytest.fixture
-def form_user():
-    return models.User(email=EMAIL, phone_number=PHONE_NUM, password=PASSWORD, username=NAME)
-
-
-@pytest.fixture
-def persist_form_user(form_user):
-    form_user.save()
-    return form_user
 
 
 @pytest.fixture
@@ -61,8 +46,8 @@ class TestCreateEventForm:
             # date_time_end cannot be equal to date_time_start
             {'title': None, 'date_time_start': DATE_TIME_START, 'date_time_end': DATE_TIME_START, 'color': COLOR},
         ])
-    def test_event_creation_form_errors(self, invalid_data, form_user):
-        form = EventCreationForm(data=invalid_data, user_id=form_user)
+    def test_event_creation_form_errors(self, invalid_data, persist_user):
+        form = EventCreationForm(data=invalid_data, user_id=persist_user)
 
         with pytest.raises(ValueError):
             if form.is_valid():
@@ -70,18 +55,18 @@ class TestCreateEventForm:
             else:
                 raise ValueError()
 
-    def test_event_created(self, valid_event_data, persist_form_user):
-        form = EventCreationForm(data=valid_event_data, user_id=persist_form_user)
+    def test_event_created(self, valid_event_data, persist_user):
+        form = EventCreationForm(data=valid_event_data, user_id=persist_user)
         new_event = form.save()
         assert Event.objects.get(pk=new_event.id)
 
-    def test_event_participant_created(self, valid_event_data, persist_form_user):
-        form = EventCreationForm(data=valid_event_data, user_id=persist_form_user)
+    def test_event_participant_created(self, valid_event_data, persist_user):
+        form = EventCreationForm(data=valid_event_data, user_id=persist_user)
         new_event = form.save()
-        assert EventParticipant.objects.get(event_id=Event.objects.get(pk=new_event.id), user_id=persist_form_user)
+        assert EventParticipant.objects.get(event_id=Event.objects.get(pk=new_event.id), user_id=persist_user)
 
-    def test_user_not_exists_when_saving_form(self, valid_event_data, form_user):
-        form = EventCreationForm(data=valid_event_data, user_id=form_user)
+    def test_user_not_exists_when_saving_form(self, valid_event_data, user0):
+        form = EventCreationForm(data=valid_event_data, user_id=user0)
         with pytest.raises(models.User.DoesNotExist):
             if form.is_valid():
                 form.save()
