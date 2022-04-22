f7184da87f1468ebf9d5dd3245365d2dd4e39ca8
danninon <danninon@gmail.com>
2022-04-22 14:08:36 +0300

    create_notificat function

--- main/utilities.py
+++ main/utilities.py
@@ -2,12 +2,17 @@ from django.conf import settings
 from django.core.mail import send_mail
 
 
-def send_site_notification(message, receiver_id):
-    """ notify the receiver by his id with the message given by the site notification """
-    print(f"notification -> {message} - {receiver_id}")  # for debug only
+def send_mail_notification(title, message, receiver_email):
+    """ send an email notification with the title and message to the receiver email given """
+    send_mail(title, message, settings.EMAIL_HOST_USER, [receiver_email])
+
+
+def create_notification(message, receiver_id):
+    """ create a notification with the receiver id the message given using the site notification """
+    print(f"notification -> {message}  -  {receiver_id}")  # for debug only
+
 
 # pre built notifications
-
 
 def send_reminder_email(message, receiver_email):
     """ default reminder email message for calendar alert """
