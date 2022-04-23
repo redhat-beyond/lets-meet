b5449b98529dbc80c65a238c6fb415c54b2798b9
Belikov <83750345+BelikovYoav@users.noreply.github.com>
2022-04-23 23:36:06 +0300

    Add create event page (#57)
    * Add create event page
    
    when submitting create_event form, a new event is created in events model
    and a new data is created with current event and
    current user in eventParticipant model.
    
    * fix event form
    
    * specifying fields and fixing issue in the save method
    
    * test the event creation form
    
    * more fixes in different files
    
    * add login required
    
    * add css anf minor fixes
    
    * removing print debugging methods
    
    * removing unused css classes
    
    * add using of the user's test fixtures
    
    * removing print debugging methods
    
    * unscoping fixtures in order to ease the using from other files
    
    * css changes and redirections
    
    * fix requested changes
    
    * form improvments
    
    - added widgets to form class meta
    - new update form
    
    * Add reminders to event page
    
    - organize files in folders
    -add event apdate page
    
    * fix changes
    
    * fix changes
    
    * fix
    
    * flake8
    
    Co-authored-by: Ravid Yael <ravid.yael.7@gmail.com>

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,4 @@
 from .class_tests.event_tests import *  # noqa: F403 F401
 from .class_tests.participant_test import *  # noqa: F403 F401
-from .class_models.meeting_models import *  # noqa: F403 F401
+from .class_tests.meeting_tests import *  # noqa: F403 F401
+from .class_tests.create_events import *  # noqa: F403 F401
