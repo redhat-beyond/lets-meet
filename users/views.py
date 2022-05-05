43839ef8e6dc08ce47db840f45471483b5f3b8a8
Tamir Sror <tamirsr@mta.ac.il>
2022-05-05 12:27:19 +0300

    Day View:
    - added day view
    - added a class for the main page
    
    Signed-off-by: Tamir Sror <tamirsr@mta.ac.il>

--- users/views.py
+++ users/views.py
@@ -3,10 +3,15 @@ from calendar import Calendar
 from users.models import User
 from events.models import Event
 from django.contrib import messages
+from django.http import JsonResponse
+from main.utilities import time_format
 from django.shortcuts import redirect, render
+from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 from users.forms import MyUserCreationForm, UserUpdateForm
 from django.contrib.auth import authenticate, login, logout
+
+# from events.models import OptionalMeetingDates
 
 cal = Calendar(6)  # 6 => means that the week start with Sunday
 
@@ -28,7 +33,7 @@ def login_page(request):
         except Exception:
             messages.error(request, "user does not exist")
         user = authenticate(request, email=email, password=password)
-        if user:
+        if user is not None:
             login(request, user)
             return redirect("home")
         else:
@@ -86,124 +91,187 @@ def update_user(request):
 
 
 @login_required(login_url="login")
-def main_page(request, date=None):
-
-    if not date:
-        year, month = None, None
-    else:
-        year, month = date.split("|")
-        year = int(year)
-        month = int(month)
-
-    cal = get_calendar_days(year, month)
-    current_month = get_current_month_name(month)
-    current_date = (datetime.now().year, datetime.now().month, datetime.now().day)
-    current_events = Event.objects.filter(eventparticipant__user_id=request.user.id)
-    table_measurements = get_table_measurements(current_events)
-    year = year if year else current_date[0]
-    week_days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
-    return render(
-        request, "user_site/home.html",
-        {'week_days': week_days, 'calendar': cal, 'month_name': current_month, 'current_year': year,
-         'next_date': get_dates(year, month, True), 'previous_date': get_dates(year, month, False),
-         'max_height': table_measurements[0], 'max_margin': table_measurements[1],
-         'max_margin_events': table_measurements[2], 'max_padding': table_measurements[3],
-         'current_date': current_date, 'current_events': current_events}
+def get_day_events(request, day, month, year):
+    day, month, year = int(day), int(month), int(year)
+    result = list(
+        Event.objects.get_all_user_day_events(
+            request.user, datetime(year, month, day)
+        ).values('title', 'date_time_start', 'date_time_end', 'color', 'description')
     )
 
+    for event in result:
+        event["date_time_end"] = time_format(event["date_time_end"]).split(" ")[1]
+        event["date_time_start"] = time_format(event["date_time_start"]).split(" ")[1]
+        event["end_hour"] = event['date_time_end'].split(":")[0]
+        event["start_hour"] = event['date_time_start'].split(":")[0]
+        event["end_minute"] = event['date_time_end'].split(":")[1]
+        event["start_minute"] = event['date_time_start'].split(":")[1]
 
-def get_dates(year=None, month=None, next=True):
-    """ return the next or previous date.
-        next: if true the next date will be returned otherwise the previous
-    """
-
-    if not year:
-        year = int(datetime.now().year)
-
-    if not month:
-        month = int(datetime.now().month)
-
-    action = 1 if next else -1
-
-    if month + action > 12:
-        year += 1
-        month -= 11
-    elif month + action < 1:
-        year -= 1
-        month += 11
-    else:
-        month += action
-
-    return f"{year}|{month}"
+    return JsonResponse(result, safe=False)
 
 
-def get_max_day_events(events):
-    """ return the maximum events count in one day over all the events given """
+class MainPageView(TemplateView):
 
-    events_count = {}
-    if len(events) == 0:
-        return 0
+    template_name = "user_site/home.html"
 
-    for event in events:
-        if not events_count.get(event.date_time_start.date()):
-            events_count[event.date_time_start.date()] = 1
+    def __init__(self, **kwargs) -> None:
+        self.user = None
+        self.year = None
+        self.month = None
+        super().__init__(**kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        
+        return super(MainPageView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        current_meetings = Event.objects.get_all_user_meetings(self.user)
+        table_measurements = self.get_table_measurements(
+            Event.objects.filter(eventparticipant__user_id=self.user)
+        )
+
+        context = {
+            'hours': range(0, 24),
+            'max_height': table_measurements[0],
+            'max_margin': table_measurements[1],
+            'max_padding': table_measurements[3],
+            'current_meetings': current_meetings,
+            'max_margin_events': table_measurements[2],
+            'month_name': self.get_current_month_name(self.month),
+            'next_date': self.get_dates(self.year, self.month, True),
+            'calendar': self.get_calendar_days(self.year, self.month),
+            'previous_date': self.get_dates(self.year, self.month, False),
+            'current_events': Event.objects.get_all_user_events(self.user),
+            # 'all_possible_dates': self.get_optional_meetings(current_meetings),
+            'current_date': (datetime.now().year, datetime.now().month, datetime.now().day),
+            'week_days': ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+        }
+
+        return context
+
+    def get(self, request, date=None):
+        self.user = request.user
+        if not date:
+            self.year, self.month = None, None
         else:
-            events_count[event.date_time_start.date()] += 1
+            year, month = date.split("|")
+            self.year = int(year)
+            self.month = int(month)
 
-    return max(events_count.values())
+        return super().get(request)
+
+    # @staticmethod
+    # def get_optional_meetings(current_meetings):
+    #     all_possible_dates = []
+
+    #     for meeting in current_meetings:
+    #         optional_meetings = OptionalMeetingDates.objects.get_all_event_dates(meeting)
+    #         for optional_meeting in optional_meetings:
+    #             event_dict = {'id': optional_meeting.event_creator_id.event_id.id,
+    #                           'title': optional_meeting.event_creator_id.event_id.title,
+    #                           'date_time_start': optional_meeting.date_time_start,
+    #                           'date_time_end': optional_meeting.date_time_end,
+    #                           'color': optional_meeting.event_creator_id.event_id.color }
+    #             all_possible_dates.append(event_dict)
+
+    #     return all_possible_dates
+
+    @staticmethod
+    def get_dates(year=None, month=None, next=True):
+        """ return the next or previous date.
+            next: if true the next date will be returned otherwise the previous
+        """
+
+        if not year:
+            year = int(datetime.now().year)
+
+        if not month:
+            month = int(datetime.now().month)
+
+        action = 1 if next else -1
+
+        if month + action > 12:
+            year += 1
+            month -= 11
+        elif month + action < 1:
+            year -= 1
+            month += 11
+        else:
+            month += action
+
+        return f"{year}|{month}"
+
+    @staticmethod
+    def get_max_day_events(events):
+        """ return the maximum events count in one day over all the events given """
+
+        events_count = {}
+        if len(events) == 0:
+            return 0
+
+        for event in events:
+            if not events_count.get(event.date_time_start.date()):
+                events_count[event.date_time_start.date()] = 1
+            else:
+                events_count[event.date_time_start.date()] += 1
+
+        return max(events_count.values())
 
 
-def get_table_measurements(events):
-    """ get each tab in the table a measurement for the positioning
+    def get_table_measurements(self, events):
+        """ get each tab in the table a measurement for the positioning
 
-        maximum row height, tab_top_positioning, event_margin_top, event_maring_bottom
-    """
+            maximum row height, tab_top_positioning, event_margin_top, event_maring_bottom
+        """
 
-    max_amount_of_events = get_max_day_events(events)  # get max events in one day
+        max_amount_of_events = self.get_max_day_events(events)  # get max events in one day
 
-    tab_top_positioning = max_amount_of_events * 2 if max_amount_of_events > 2 else 4
-    maximum_row_height = max_amount_of_events * 5 if max_amount_of_events > 2 else 13
+        tab_top_positioning = max_amount_of_events * 2 if max_amount_of_events > 2 else 4
+        maximum_row_height = max_amount_of_events * 5 if max_amount_of_events > 2 else 13
 
-    maximum_row_height = min(maximum_row_height, 30)
-    tab_top_positioning = min(tab_top_positioning, 13)
+        maximum_row_height = min(maximum_row_height, 30)
+        tab_top_positioning = min(tab_top_positioning, 13)
 
-    event_margin_top = min(tab_top_positioning, 13) - 4
-    event_maring_bottom = event_margin_top + 0.5
+        event_margin_top = min(tab_top_positioning, 13) - 4
+        event_maring_bottom = event_margin_top + 0.5
 
-    return (maximum_row_height, tab_top_positioning, event_margin_top, event_maring_bottom)
+        return (maximum_row_height, tab_top_positioning, event_margin_top, event_maring_bottom)
 
+    @staticmethod
+    def get_current_month_name(month=None):
+        """ get the current month name.
 
-def get_current_month_name(month=None):
-    """ get the current month name.
+            if month is given then get the name of that month
+            otherwise get this month name
+        """
+        current_date = datetime.now()
+        if month:
+            current_date = datetime(current_date.year, month, current_date.day)
+        return current_date.strftime("%B")
 
-        if month is given then get the name of that month
-        otherwise get this month name
-    """
-    current_date = datetime.now()
-    if month:
-        current_date = datetime(current_date.year, month, current_date.day)
-    return current_date.strftime("%B")
+    @staticmethod
+    def get_calendar_days(year=None, month=None):
+        """ return a matrix of dates. Each sublist is a week in the month, each week has 7 dates.
+            This list will include dates from the month before if the current month hasn't started with sunday """
 
+        current_week = list()
+        calendar_dates = list()
+        current_date = datetime.now()
+        if not year:
+            year = current_date.year
+        if not month:
+            month = current_date.month
 
-def get_calendar_days(year=None, month=None):
-    """ return a matrix of dates. Each sublist is a week in the month, each week has 7 dates.
-        This list will include dates from the month before if the current month hasn't started with sunday """
-
-    current_week = list()
-    calendar_dates = list()
-    current_date = datetime.now()
-    if not year:
-        year = current_date.year
-    if not month:
-        month = current_date.month
-
-    # iterate over the list of all the current month dates.
-    # divide the list to each week and return the resulting matrix
-    for index, date_index in enumerate(cal.itermonthdays3(year, month)):
-        current_week.append(date_index)
-        if (index + 1) != 1 and (index + 1) % 7 == 0:
-            calendar_dates.append(current_week)
-            current_week = list()
-    calendar_dates.append(current_week)
-    return calendar_dates
+        # iterate over the list of all the current month dates.
+        # divide the list to each week and return the resulting matrix
+        for index, date_index in enumerate(cal.itermonthdays3(year, month)):
+            current_week.append(date_index)
+            if (index + 1) != 1 and (index + 1) % 7 == 0:
+                calendar_dates.append(current_week)
+                current_week = list()
+        calendar_dates.append(current_week)
+        return calendar_dates
