7982db7f221645e59eab9c48eb0ce2409731fe9a
MortarDefender <63960808+MortarDefender@users.noreply.github.com>
2022-05-08 01:01:06 +0300

    Notification (#73)
    * basic notification skeleton
    
    * notification model+tests and small improvements on reminder as part of application reminder
    
    * fixed test
    
    * added utilities
    
    * removed files
    
    * create_notificat function
    
    * Meta Class And Flake8:
    
    - added meta class to reminders and notification
    - remove unneeded linex, comments and functions
    - fixed some tests
    
    Signed-off-by: Tamir Sror <tamirsr@mta.ac.il>
    
    * Comments And ForEach:
    
    - comments and unneeded functions removed
    - added inline for, for a couple of functions in the tests
    
    Signed-off-by: Tamir Sror <tamirsr@mta.ac.il>
    
    * merge
    
    Co-authored-by: danninon <danninon@gmail.com>

--- reminders/admin.py
+++ reminders/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Reminder
+from .models import Reminder, Notification
 
 admin.site.register(Reminder)
+admin.site.register(Notification)
---
+++ reminders/migrations/0003_add_notification_test_data.py
@@ -0,0 +1,47 @@
+from django.db import migrations, transaction
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('reminders', '0001_initial'),
+        ('reminders', '0002_add_reminder_test_data'),
+        ('users', '0003_createsuperuser'),
+        ('events', '0004_add_event_participant_test_data')
+    ]
+
+    def generate_notification_test_data(apps, schema_editor):
+        from reminders.models import Notification
+        from events.models import EventParticipant
+        from django.utils import timezone
+        from datetime import datetime
+        JOIN_MEETING = 'Joined Meeting in {} minutes'
+
+        notification_data = [
+            (EventParticipant.objects.get(event_id__title="event1", user_id__username="testUser1"),
+             datetime(2022, 12, 25, 11, 11, 11, 0, tzinfo=timezone.utc),
+             datetime(2022, 12, 24, 11, 11, 11, 0, tzinfo=timezone.utc),
+             JOIN_MEETING.format(25),
+             ),
+
+            (EventParticipant.objects.get(event_id__title="event2", user_id__username="testUser3"),
+             datetime(2033, 3, 24, 11, 11, 11, 0, tzinfo=timezone.utc),
+             datetime(2032, 2, 24, 11, 11, 11, 0, tzinfo=timezone.utc),
+             JOIN_MEETING.format(35),
+             ),
+
+            (EventParticipant.objects.get(event_id__title="event1", user_id__username="testUser2"),
+             None,
+             datetime(2025, 1, 1, 11, 11, 11, 0, tzinfo=timezone.utc),
+             JOIN_MEETING.format(45),
+             ),
+        ]
+
+        with transaction.atomic():
+            for participant_id, seen_time, sent_time, message in notification_data:
+                notification = Notification(participant_id=participant_id, seen_time=seen_time, sent_time=sent_time,
+                                            message=message)
+                notification.save()
+
+    operations = [
+        migrations.RunPython(generate_notification_test_data),
+    ]
