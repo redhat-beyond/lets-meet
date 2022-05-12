acdb46df3898136c46478ce0f01952f0445cd2a2
Ron H <hachmonron@gmail.com>
2022-05-12 16:54:55 +0300

    Adding test
    adding a test checking if all file related to event are shown,
    and checking if they are donwloadable from the page
    
    only Rebase
    
    only rebase
    
    Revert "only Rebase"
    
    This reverts commit 8fb98781bb953123108e8833cdb2f94cbfbfc851.

--- eventfiles/class_tests/event_file_views_tests.py
+++ eventfiles/class_tests/event_file_views_tests.py
@@ -12,8 +12,10 @@ FILE_CREATION_HTML_PATH = "file/event_files.html"
 FILE_DELETE_URL = "/file/event_file/1/delete"
 FILE_CREATED_BY_USER2 = 3
 FILE_CREATED_BY_EVENT_CREATOR = 1
+EVENT_ID = 1
 DELETE_USER2_FILE_URL = f'{FILE_DELETE_URL}/{FILE_CREATED_BY_USER2}'
 DELETE_EVENT_CREATOR_FILE_URL = f'{FILE_DELETE_URL}/{FILE_CREATED_BY_EVENT_CREATOR}'
+DOWNLOAD_FILE_FROM_EVENT_URL = f'download/?event={EVENT_ID}&file_name='
 
 
 @pytest.mark.django_db
@@ -56,20 +58,31 @@ class TestEventFileFormView:
         response = client.get(url_path)
         assert response.status_code == status_code
 
-    def test_unauthorized_user_redirected_to_main_page(self, client):
+    def test_unauthorized_user_redirected_to_login_page(self, client):
         response = client.get(f'{FILE_CREATION_URL}/1')
         assert response.status_code == 302
         response = client.get(response.url)
         assert response.status_code == 200
         assertTemplateUsed(response, 'login/register_login.html')
 
+    def test_all_event_files_are_shown_and_downloadable(self, sign_in_with_user2, client):
+        response = client.get(reverse("event_files", args=[EVENT_ID]))
+        char_content = response.content.decode(response.charset)
+        all_file_in_event1 = EventFile.objects.get_files_by_event(EVENT_ID)
+        for file in all_file_in_event1:
+            assert f'{file}' in char_content
+            assert f'{DOWNLOAD_FILE_FROM_EVENT_URL}{file}' in char_content
+            response = client.get('/file/event_file/download/', {'event': EVENT_ID, 'file_name': f'{file}'})
+            assert response.status_code == 200
+            downloaded_file = self.get_file_name_from_response(response)
+            assert f'{file}' in downloaded_file
+
     def test_delete_button_visible_for_file_user_created(self, sign_in_with_user2, client):
         """ test that an ordinary user can delete only files he created """
-        response = client.get(reverse("event_files", args=[1]))
+        response = client.get(reverse("event_files", args=[EVENT_ID]))
         char_content = response.content.decode(response.charset)
         found_file3 = re.search(f'delete testFile{FILE_CREATED_BY_USER2}', char_content)
         found_file1 = re.search(f'delete testFile{FILE_CREATED_BY_EVENT_CREATOR}', char_content)
-        print(char_content)
         assert found_file3 and not found_file1
 
     def test_unauthorized_user_delete(self, sign_in_with_user2, client):
@@ -82,7 +95,7 @@ class TestEventFileFormView:
 
     def test_all_delete_buttons_visible_for_event_creator(self, sign_in_with_creator, client):
         """ test that the event creator can delete all files """
-        response = client.get(reverse("event_files", args=[1]))
+        response = client.get(reverse("event_files", args=[EVENT_ID]))
         char_content = response.content.decode(response.charset)
         found_file3 = re.search(f'delete testFile{FILE_CREATED_BY_USER2}', char_content)
         found_file1 = re.search(f'delete testFile{FILE_CREATED_BY_EVENT_CREATOR}', char_content)
@@ -105,3 +118,7 @@ class TestEventFileFormView:
         response = client.get(response.url)
         assert response.status_code == 200
         assertTemplateUsed(response, FILE_CREATION_HTML_PATH)
+
+    def get_file_name_from_response(self, response):
+        header = response.headers
+        return header['Content-Disposition']
