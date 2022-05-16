704b3ed277badaa81711d63c17283b9303396d80
YaelD <63968945+YaelD@users.noreply.github.com>
2022-05-17 01:35:58 +0300

    Create meeting forms and views (#84)
    * Add create event page
    
    when submitting create_event form, a new event is created in events model
    and a new data is created with current event and
    current user in eventParticipant model.
    
    * fix event form
    
    * test the event creation form
    
    * more fixes in different files
    
    * add login required
    
    * add css anf minor fixes
    
    * save work before merge
    
    * Working on the meeting feature
    
    * Adding UI to create meeting feature:
    
    - Adding html, js and css files for create meeting feature.
    - Adding js file for adding forms dynamically to the html file.
    
    Signed-off-by: Yael Davidov <yael.davidov97@gmail.com>
    
    * Adding forms and views files to create-meeting feature
    
    - Adding forms of participants and optional meeting dates
    - Adding view function to handle the meeting creation
    - Added formsets for optional meeting dates
    - Added formsets for participants
    
    Signed-off-by: Yael Davidov <yael.davidov97@gmail.com>
    
    * Class Meta:
    
    - adding unique constraints to the event model
    - adding unique constraints to the optional meeting model
    - adding tests for row duplication for both models
    
    Signed-off-by: Yael Davidov <yael.davidov97@gmail.com>
    
    * Fixing requested changes
    
    - Changing some fields in css and html files
    - Changing some conditions syntax in views file
    
    * Create a class for create meeting view
    
    - Adding a class for create meeting view
    - Adding all the functions that related to the meeting view in the class
    - Changing the url accordingly
    
    Signed-off-by: Yael Davidov <yael.davidov97@gmail.com>
    
    * Changing boolen variable in forms file
    
    - Changing boolen variable in BaseParticipantFormSet class
    
    * Removing the uniqueness of events
    
    - Removing class Meta from event model
    - Removing the uniqueness tests of events in the event_tests file
    - Removing the unique constraint in events/migrations/0001_initial file
    
    * Fixing flake8 errors
    
    - Fixing flake8 errors
    
    Co-authored-by: BelikovYoav <yoavbl@mta.ac.il>
    Co-authored-by: Ravid Yael <ravid.yael.7@gmail.com>

--- events/forms.py
+++ events/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from users.models import User
-from django.forms import ModelForm, Textarea
+from django.utils import timezone
+from django.forms import ModelForm, Textarea, BaseFormSet
 from .class_models.event_models import Event
 from .class_models.participant_model import EventParticipant
+from .class_models.meeting_models import OptionalMeetingDates
+from django.core.exceptions import ValidationError
 
 
 class DataTimePickerInput(forms.DateTimeInput):
@@ -54,3 +57,77 @@ class EventUpdateForm(ModelForm):
                 format="%Y-%m-%dT%H:%M",
             )
         }
+
+
+class BaseOptionalMeetingDateFormSet(BaseFormSet):
+    def clean(self):
+        """checks that no two optional meetings dates with the same dates"""
+        if any(self.errors):
+            # Don't bother validating the formset unless each form is valid on its own
+            return
+
+        dates = []
+        for form in self.forms:
+            if form.cleaned_data.get('date_time_start') and form.cleaned_data.get('date_time_end'):
+                start_date = form.cleaned_data.get('date_time_start')
+                end_date = form.cleaned_data.get('date_time_end')
+                if (start_date, end_date) in dates:
+                    raise ValidationError("The optional meeting dates should be different")
+                if start_date < timezone.now() or end_date < timezone.now():
+                    raise ValidationError("Optional meeting dates cannot be in the past")
+                dates.append((start_date, end_date))
+            elif not form.cleaned_data.get('date_time_start') and not form.cleaned_data.get('date_time_end'):
+                pass
+            else:
+                raise ValidationError("Some date fields are blank")
+
+
+class OptionalMeetingDateForm(ModelForm):
+
+    class Meta:
+        model = OptionalMeetingDates
+        exclude = ['event_creator_id']
+        fields = ['date_time_start', 'date_time_end']
+        widgets = {
+            'date_time_start': forms.DateInput(
+                attrs={"type": "datetime-local"},
+                format="%Y-%m-%dT%H:%M",
+            ),
+            'date_time_end': forms.DateInput(
+                attrs={"type": "datetime-local"},
+                format="%Y-%m-%dT%H:%M",
+            )
+        }
+
+
+class BaseParticipantFormSet(BaseFormSet):
+
+    def __init__(self, *args, **kwargs):
+        self.user_id = kwargs.pop('user_id')
+        super(BaseParticipantFormSet, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        """ Checking if there is at least one participant """
+        if any(self.errors):
+            # Don't bother validating the formset unless each form is valid on its own
+            return
+
+        is_at_least_one_participant = False
+        if(len(self.forms) >= 1):
+            for form in self.forms:
+                email_participant = form.cleaned_data.get('participant_email')
+                if email_participant:
+                    try:
+                        user_instance = User.objects.get(email=email_participant)
+                        creator = User.objects.get(email=self.user_id)
+                        if user_instance == creator:
+                            raise ValidationError("You can't add yourself as participant")
+                        is_at_least_one_participant = True
+                    except User.DoesNotExist:
+                        raise ValidationError(f"There is not user with the email: {email_participant}")
+        if not is_at_least_one_participant:
+            raise ValidationError("You have to enter at least one participant in the meeting")
+
+
+class ParticipantForm(forms.Form):
+    participant_email = forms.EmailField()
---
+++ events/static/meetings/create_meeting.js
@@ -0,0 +1,48 @@
+let total_meeting_dates_forms;
+let total_participant_forms;
+const addMoreOptionalDatesBtn = document.getElementById('add-more-optional-meeting-dates')
+addMoreOptionalDatesBtn.addEventListener('click', add_optional_meeting_dates_form)
+const addMoreBtn = document.getElementById('add-more-meeting-paricipants')
+addMoreBtn.addEventListener('click', add_participant_form)
+
+
+function add_optional_meeting_dates_form(event) {
+    if(event){
+        event.preventDefault()
+    }
+    add_form('optional-meeting-dates', 'optional_meetings')
+}
+
+function add_participant_form(event) {
+    if(event){
+        event.preventDefault()
+    }
+    add_form('meeting-participants', 'participants')
+}
+
+function add_form(form_name, form_prefix){
+    const totalNewFroms = document.getElementById(`id_${form_prefix}-TOTAL_FORMS`)
+    if(totalNewFroms.getAttribute('value') == 10){
+        return 
+    }
+    const currentForms= document.getElementsByClassName(`${form_name}-form`)
+    const currentFormCount = currentForms.length
+    const formList = document.getElementById(`${form_name}-form-list`)
+    const copyEmptyFormElement = document.getElementById(`empty-${form_name}-form`).cloneNode(true)
+    copyEmptyFormElement.setAttribute('class', `${form_name}-form`)
+    copyEmptyFormElement.setAttribute('id', `form-${currentFormCount}`)
+    const regex = new RegExp('__prefix__', 'g')
+    copyEmptyFormElement.innerHTML = copyEmptyFormElement.innerHTML.replace(regex, currentFormCount)
+    totalNewFroms.setAttribute('value', currentFormCount + 1)
+    formList.append(copyEmptyFormElement)
+}
+
+function set_total_meeting_forms() {
+    const totalNewOptionalDatesFroms = document.getElementById('id_optional_meetings-TOTAL_FORMS')
+    totalNewOptionalDatesFroms.setAttribute('value', total_meeting_dates_forms)
+}
+
+function set_total_participant_forms() {
+    const totalNewParticipantFroms = document.getElementById('id_participants-TOTAL_FORMS')
+    totalNewParticipantFroms.setAttribute('value', total_participant_forms)
+}
