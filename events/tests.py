b566d9f26dccc3b6adac2ad60788533cc0ad6870
Ron Hachmon <88894070+RonHachmon@users.noreply.github.com>
2022-05-20 08:53:44 +0300

    Test for meeting form (#91)
    * Test for meeting form
    
    Testing:
    - form created with valid input
    - bad input to participant form : no email,bad email,event creator email included
    - bad input meeting : past dates, partial input , dates arent unique
    - basic url test
    
    * static method
    
    * re-using constant
    
    * indetation
    
    * test and constant
    
    adding test for unsigend user redirection and using constant

--- events/tests.py
+++ events/tests.py
@@ -4,3 +4,4 @@ from .class_tests.meeting_tests import *  # noqa: F403 F401
 from .class_tests.possible_participant_tests import *  # noqa: F403 F401
 from .class_tests.create_events import *  # noqa: F403 F401
 from .class_tests.planner_tests import *  # noqa: F403 F401
+from .class_tests.create_meeting_form import *  # noqa: F403 F401
