0366e4ba43619e33c1d01c0d5eef298e619b0eed
Ravid Yael <ravid.yael.7@gmail.com>
2022-05-22 22:03:36 +0300

    style: removing some unnecessary varibles

--- events/class_tests/test_create_event_views.py
+++ events/class_tests/test_create_event_views.py
@@ -1,53 +1,48 @@
 import pytest
 from pytest_django.asserts import assertTemplateUsed
-from events.class_tests.create_events import DATE_TIME_START, DATE_TIME_END, COLOR, TITLE
 from reminders.class_models.reminder_models import ReminderType
-from events.models import Event, Colors
-
-CREATE_EVENT_URL = "/event/create/"
-CREATE_EVENT_TEMPLATE = 'events/create_event.html'
-LOGIN_PAGE_TEMPLATE = 'login/register_login.html'
+from events.models import Event
 
 
 @pytest.mark.django_db
 class TestCreateEventViews:
 
     def test_logged_user_success_enter_the_create_event_page(self, sign_in, client):
-        response = client.get(path=CREATE_EVENT_URL)
+        response = client.get(path=pytest.CREATE_EVENT_URL)
         assert response.status_code == 200
-        assertTemplateUsed(response, CREATE_EVENT_TEMPLATE)
+        assertTemplateUsed(response, pytest.CREATE_EVENT_TEMPLATE)
 
     def test_unlogged_user_fail_to_enter_the_create_event_page(self, client):
-        response = client.get(path=CREATE_EVENT_URL)
+        response = client.get(path=pytest.CREATE_EVENT_URL)
         assert response.status_code == 302
         response = client.get(response.url)
-        assertTemplateUsed(response, LOGIN_PAGE_TEMPLATE)
+        assertTemplateUsed(response, pytest.LOGIN_PAGE_TEMPLATE)
 
     def test_logged_user_success_create_event(self, sign_in, valid_event_data_and_reminder, client):
-        assert not Event.objects.filter(title=TITLE)
-        client.post(path=CREATE_EVENT_URL, data=valid_event_data_and_reminder)
-        assert Event.objects.get(title=TITLE)
+        assert not Event.objects.filter(title=pytest.valid_event_title)
+        client.post(path=pytest.CREATE_EVENT_URL, data=valid_event_data_and_reminder)
+        assert Event.objects.get(title=pytest.valid_event_title)
 
     @pytest.mark.parametrize("expected_error_message,invalid_data", [
         ('Title cannot be blank',
-            {'title': "", 'date_time_start': pytest.date_time_start,
-                'date_time_end': pytest.date_time_end, 'color': Colors.BLACK,
+            {'title': "", 'date_time_start': pytest.event_date_time_start,
+                'date_time_end': pytest.event_date_time_end, 'color': pytest.valid_event_color,
                 'date_time': pytest.reminder_date_time, 'method': ReminderType.EMAIL}),
         ('Starting date cannot be blank',
-            {'title': TITLE, 'date_time_start': "",
-                'date_time_end': DATE_TIME_END, 'color': COLOR,
+            {'title': pytest.valid_event_title, 'date_time_start': "",
+                'date_time_end': pytest.valid_event_date_time_end, 'color': pytest.valid_event_color,
                 'date_time': pytest.reminder_date_time, 'method': ReminderType.EMAIL}),
         ('Ending date cannot be blank',
-            {'title': TITLE, 'date_time_start': DATE_TIME_START,
-                'date_time_end': "", 'color': COLOR,
+            {'title': pytest.valid_event_title, 'date_time_start': pytest.valid_event_date_time_start,
+                'date_time_end': "", 'color': pytest.valid_event_color,
                 'date_time': pytest.reminder_date_time, 'method': ReminderType.EMAIL}),
         ('2012-12-12 12:12:00 must be smaller than 2012-11-11 11:11:00',
-            {'title': TITLE, 'date_time_start': DATE_TIME_END,
-                'date_time_end': DATE_TIME_START, 'color': COLOR,
+            {'title': pytest.valid_event_title, 'date_time_start': pytest.valid_event_date_time_end,
+                'date_time_end': pytest.valid_event_date_time_start, 'color': pytest.valid_event_color,
                 'date_time': pytest.reminder_date_time, 'method': ReminderType.EMAIL}),
-        ('2012-11-11 11:11:00 must be smaller than 2012-11-11 11:11:00',
-            {'title': TITLE, 'date_time_start': DATE_TIME_START,
-                'date_time_end': DATE_TIME_START, 'color': COLOR,
+        ('2012-12-12 12:12:00 must be smaller than 2012-11-11 11:11:00',
+            {'title': pytest.valid_event_title, 'date_time_start': pytest.valid_event_date_time_end,
+                'date_time_end': pytest.valid_event_date_time_start, 'color': pytest.valid_event_color,
                 'date_time': pytest.reminder_date_time, 'method': ReminderType.EMAIL})
         ], ids=[
            "title is blank",
@@ -58,6 +53,6 @@ class TestCreateEventViews:
         ]
      )
     def test_event_creation_form_errors(self, client, sign_in, invalid_data, expected_error_message):
-        response = client.post(path=CREATE_EVENT_URL, data=invalid_data)
+        response = client.post(path=pytest.CREATE_EVENT_URL, data=invalid_data)
         errors = response.context["event_form"].errors.as_data()
         assert expected_error_message in f'{errors}'
--- events/conftest.py
+++ events/conftest.py
@@ -2,22 +2,14 @@ import pytest
 from django.utils import timezone
 from django.utils.datetime_safe import datetime
 from events.class_models.event_models import Colors
-from events.class_tests.create_events import TITLE
 from reminders.class_models.reminder_models import ReminderType
+from events.class_tests.create_events import DATE_TIME_START, DATE_TIME_END, COLOR, TITLE
 
 
 # define constants
 def pytest_configure():
-    pytest.invalid_method = "undefined"
+
     pytest.current_date = datetime.now()
-    pytest.valid_method = ReminderType.EMAIL
-    pytest.message = "Joined Meeting in 50 minutes"
-    pytest.valid_date_time = datetime(
-        pytest.current_date.year, pytest.current_date.month, pytest.current_date.day + 1, 12, 0, 0, tzinfo=timezone.utc
-    )
-    pytest.invalid_date_time = datetime(
-        pytest.current_date.year - 1, pytest.current_date.month, pytest.current_date.day, 12, 0, 0, tzinfo=timezone.utc
-    )
 
     pytest.event_date_time_end = datetime(
         pytest.current_date.year, pytest.current_date.month, pytest.current_date.day + 1, 14, 0, 0, tzinfo=timezone.utc
@@ -25,16 +17,15 @@ def pytest_configure():
     pytest.event_date_time_start = datetime(
         pytest.current_date.year, pytest.current_date.month, pytest.current_date.day + 1, 13, 0, 0, tzinfo=timezone.utc
     )
-    pytest.reminder_date_time = datetime(
-        pytest.current_date.year, pytest.current_date.month, pytest.current_date.day + 1, 14, 0, 0, tzinfo=timezone.utc
-    )
 
-    pytest.reminder_creation_url = '/event/create/'
-    pytest.reminder_update_url = '/event/update/{}'.format(1)
-    pytest.reminder_creation_html_path = "events/create_event.html"
+    pytest.CREATE_EVENT_URL = "/event/create/"
+    pytest.CREATE_EVENT_TEMPLATE = 'events/create_event.html'
+    pytest.LOGIN_PAGE_TEMPLATE = 'login/register_login.html'
 
-    pytest.exist_reminder_error = 'reminder already exists'
-    pytest.past_date_time_error = "date time should be bigger than the current date_time"
+    pytest.valid_event_date_time_start = DATE_TIME_START
+    pytest.valid_event_date_time_end = DATE_TIME_END
+    pytest.valid_event_color = COLOR
+    pytest.valid_event_title = TITLE
 
 
 @pytest.fixture
