0861ac1b3d240f7dd06b6a914a45af5dc3a98757
Ravid Yael <ravid.yael.7@gmail.com>
2022-05-23 16:20:59 +0300

    Style: better use of pytest configurations

--- events/conftest.py
+++ events/conftest.py
@@ -4,13 +4,6 @@ from django.utils.datetime_safe import datetime
 from events.class_models.event_models import Colors
 from reminders.class_models.reminder_models import ReminderType
 
-TITLE = 'new_form'
-LOCATION = 'new_form_location'
-DESCRIPTION = 'new_form_description'
-DATE_TIME_END = "2012-12-12 12:12"
-DATE_TIME_START = "2012-11-11 11:11"
-COLOR = Colors.BLACK
-
 
 # define constants
 def pytest_configure():
@@ -24,14 +17,14 @@ def pytest_configure():
         pytest.current_date.year, pytest.current_date.month, pytest.current_date.day + 1, 13, 0, 0, tzinfo=timezone.utc
     )
 
-    pytest.CREATE_EVENT_URL = "/event/create/"
-    pytest.CREATE_EVENT_TEMPLATE = 'events/create_event.html'
-    pytest.LOGIN_PAGE_TEMPLATE = 'login/register_login.html'
+    pytest.create_event_url = "/event/create/"
+    pytest.create_event_template = 'events/create_event.html'
+    pytest.login_page_template = 'login/register_login.html'
 
-    pytest.valid_event_date_time_start = DATE_TIME_START
-    pytest.valid_event_date_time_end = DATE_TIME_END
-    pytest.valid_event_color = COLOR
-    pytest.valid_event_title = TITLE
+    pytest.valid_event_date_time_start = "2012-11-11 11:11"
+    pytest.valid_event_date_time_end = "2012-12-12 12:12"
+    pytest.valid_event_color = Colors.BLACK
+    pytest.valid_event_title = 'new_form'
 
 
 @pytest.fixture
@@ -46,6 +39,6 @@ def sign_in(client, signed_up_user_details):
 
 @pytest.fixture
 def valid_event_data_and_reminder():
-    return {'title': TITLE, 'date_time_start': pytest.event_date_time_start,
-            'date_time_end': pytest.event_date_time_end, 'color': Colors.BLACK,
+    return {'title':  pytest.valid_event_title, 'date_time_start': pytest.event_date_time_start,
+            'date_time_end': pytest.event_date_time_end, 'color': pytest.valid_event_color,
             'date_time': pytest.reminder_date_time, 'method': ReminderType.EMAIL}
